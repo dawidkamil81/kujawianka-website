b3f56af9890d366a8115f7d2156f049159413e15
CycekMast3r <hubert2004s@wp.pl>
2025-12-22 15:47:40 +0100

    refactor header

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "../../components/Header";
+import Header from "../../components/layout/Header";
 import Footer from "../../components/Footer";
 
 export const metadata = {
